4175a2b9842c6520deed473cd726d8a08aaf2c66
HubCodes <krlrhkstk12@gmail.com>
2019-04-17 14:01:05 +0900

    실행 가능한 애플리케이션

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
 import "reflect-metadata";
+import dotenv from "dotenv";
+dotenv.config();
+
+import { container } from "./inversify.config";
+import injectableList from "./injectableList";
+import { App } from "./api";
+
+const app = container.get<App>(injectableList.Application);
+
+app.listen(8080);
