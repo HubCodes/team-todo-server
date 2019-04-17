53d3dd5577ff5072561c27f39f6b07bda8c086ab
HubCodes <krlrhkstk12@gmail.com>
2019-04-17 14:00:59 +0900

    class App: 애플리케이션 생명주기 관리

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,1 +1,26 @@
 import express from "express";
+import http from "http";
+import { injectable, inject } from "inversify";
+import injectableList from "../injectableList";
+import { AuthenticationRouter } from "./authentication";
+
+@injectable()
+export class App {
+  private app: express.Express;
+  private server: http.Server;
+
+  public constructor(
+    @inject(injectableList.AuthenticationRouter) private authenticationRouter: AuthenticationRouter
+  ) {
+    this.app = express();
+    this.server = http.createServer(this.app);
+    this.app.use(express.json());
+    this.app.use("/", this.authenticationRouter.getExpressRouter());
+  }
+
+  public listen(port: number | string) {
+    this.server.listen(port, () => {
+      console.info(`[INFO] Server listening at ${port}`);
+    });
+  }
+}
