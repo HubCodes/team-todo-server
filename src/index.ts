765f54350d6077a33f9fd7ec6d6ce0edb7e6e15c
HubCodes <krlrhkstk12@gmail.com>
2019-04-17 15:34:53 +0900

    PersistentMap 저장 로직 추가

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,22 @@ dotenv.config();
 import { container } from "./inversify.config";
 import injectableList from "./injectableList";
 import { App } from "./api";
+import { todoStore } from "./repository/TodoKeyValueStore";
+import { userStore } from "./repository/UserKeyValueStore";
 
 const app = container.get<App>(injectableList.Application);
 
 app.listen(8080);
+
+function onExit() {
+  todoStore.save();
+  userStore.save();
+
+  process.exit();
+}
+
+process.on("exit", onExit);
+process.on("SIGINT", onExit);
+process.on("SIGUSR1", onExit);
+process.on("SIGUSR2", onExit);
+process.on("uncaughtException", onExit);
