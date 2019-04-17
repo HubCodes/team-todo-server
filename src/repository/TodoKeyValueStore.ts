8bac84ea7f674099d159477b156ffbde20207795
HubCodes <krlrhkstk12@gmail.com>
2019-04-17 13:51:18 +0900

    K-V store 모듈화

---
+++ src/repository/TodoKeyValueStore.ts
@@ -0,0 +1,3 @@
+import { UserID, User } from "../entity/User";
+
+export const todoStore: Map<UserID, User> = new Map();
---
+++ src/repository/UserKeyValueStore.ts
@@ -0,0 +1,3 @@
+import { UserID, User } from "../entity/User";
+
+export const userStore: Map<UserID, User> = new Map();
